8e803e1d39a3c506e9d8fa743c3aa8614330fc81
Olha_Kadeeva <o.kadeeva.n@gmail.com>
2024-06-05 16:33:40 +0200

    fix all

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,9 @@
 function getElementWidth(content, padding, border) {
-  return content + padding * 2 + border * 2;
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
